8ed187941c15db4ac5422483518dc4d3771b7631
David Spreekmeester <david@grrr.nl>
2015-04-21 17:48:51 +0200

    Added buffer disks mounting commands for Ubuntu

---
+++ mount-external-buffer.fish
@@ -0,0 +1,3 @@
+function mount-externe-buffer
+	sudo mount -t ext4 -o rw,group,exec,auto,user /dev/sdb1 /mnt/externe-buffer-hd/
+end
---
+++ mount-small-buffer.fish
@@ -0,0 +1,3 @@
+function mount-kleine-buffer
+	sudo mount -t ext4 -o rw,group,exec,auto,user /dev/sdc1 /mnt/kleine-buffer
+end
