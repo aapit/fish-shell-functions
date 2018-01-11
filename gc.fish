d6c4175ed0c7ab76525483b5a306e91eb022b4ae
David Spreekmeester <david@grrr.nl>
2018-01-11 17:26:09 +0100

    Added ability to add flags to git commit

--- gc.fish
+++ gc.fish
@@ -1,3 +1,8 @@
+# Defined in /var/folders/vq/9d__bvq56dnbxycrfmb9kj1r0000gn/T//fish.q9hiUd/gc.fish @ line 2
 function gc
-	git commit -m $argv
+	if string match -q (string sub -l 1 -- $argv) "-"
+        git commit $argv
+    else
+	    git commit -m $argv
+    end
 end
