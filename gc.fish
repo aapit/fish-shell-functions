88b5e4adb738620c32272aa560743a5524bd3278
David Spreekmeester <david@grrr.nl>
2018-03-01 16:24:49 +0100

    Added verbose flag to Git commit

--- gc.fish
+++ gc.fish
@@ -1,7 +1,8 @@
+# Defined in /var/folders/vq/9d__bvq56dnbxycrfmb9kj1r0000gn/T//fish.5VP0M8/gc.fish @ line 2
 function gc
-    if string match -q (string sub -l 1 -- $argv) "-"
-        git commit $argv
+	if string match -q (string sub -l 1 -- $argv) "-"
+        git commit --verbose $argv
     else
-        git commit -m $argv
+        git commit --verbose -m $argv
     end
 end
